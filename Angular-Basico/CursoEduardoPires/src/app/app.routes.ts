fc7a24ae835d5a81f096270a8ab47211abb38500
Brspontes <brian.robert16@hotmail.com>
2020-03-31 18:01:19 -0300

    trabalhando com rotas e adicionando novos components

---
+++ Angular-Basico/CursoEduardoPires/src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+import { SobreComponent } from './institucional/sobre/sobre.component';
+import { ContatoComponent } from './institucional/contato/contato.component';
+import { HomeComponent } from './navegacao/home/home.component';
+
+
+export const rootRouterConfig: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'contato', component: ContatoComponent},
+    {path: 'sobre', component: SobreComponent},
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    
+];
