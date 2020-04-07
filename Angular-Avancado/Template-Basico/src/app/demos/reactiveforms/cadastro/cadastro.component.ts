197d2e4ab5ee93d08d1bebefeaf3da87b4a35748
Brspontes <brian.robert16@hotmail.com>
2020-04-07 18:05:50 -0300

    Validação de formulários

--- Angular-Avancado/Template-Basico/src/app/demos/reactiveforms/cadastro/cadastro.component.ts
+++ Angular-Avancado/Template-Basico/src/app/demos/reactiveforms/cadastro/cadastro.component.ts
@@ -1,6 +1,6 @@
 import { Usuario } from './models/Usuario';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
@@ -11,20 +11,26 @@ export class CadastroComponent implements OnInit {
 
   CadastroForm: FormGroup;
   Usuario: Usuario;
+  formResult: string = '';
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
     this.CadastroForm = this.fb.group({
-      nome: [''],
+      nome: ['', Validators.required],
       cpf: [''],
-      email: [''],
+      email: ['', [Validators.required, Validators.email]],
       senha: [''],
       confirmasenha: ['']
     });
   }
 
   adicionarUsuario(){
-    this.Usuario = Object.assign({}, this.Usuario, this.CadastroForm.value);
+    if(this.CadastroForm.dirty && this.CadastroForm.valid){
+      this.Usuario = Object.assign({}, this.Usuario, this.CadastroForm.value);
+      this.formResult = JSON.stringify(this.CadastroForm.value);
+    } else{
+      this.formResult = "Não submeteu";
+    }
   }
 }
