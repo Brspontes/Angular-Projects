32d4cda8d0e7fe03ae232546856b3423f7eb5669
Brspontes <brian.robert16@hotmail.com>
2020-04-15 17:26:02 -0300

    Route Trace

--- Angular-Avancado/Template-Basico/src/app/app.routes.ts
+++ Angular-Avancado/Template-Basico/src/app/app.routes.ts
@@ -16,7 +16,7 @@ const rootRouterConfig: Routes = [
 
 @NgModule({
     imports:[
-        RouterModule.forRoot(rootRouterConfig)
+        RouterModule.forRoot(rootRouterConfig, { enableTracing: false })
     ],
     exports:[
         RouterModule
