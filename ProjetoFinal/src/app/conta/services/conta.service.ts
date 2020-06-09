b3d8b6fb51f6017174907038850c513eed65c41a
Brspontes <brian.robert16@hotmail.com>
2020-06-09 18:52:33 -0300

    comunicação com backend post

--- ProjetoFinal/src/app/conta/services/conta.service.ts
+++ ProjetoFinal/src/app/conta/services/conta.service.ts
@@ -1,14 +1,24 @@
 import { Usuario } from './../models/usuario';
 import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError, map } from 'rxjs/operators'
+import { BaseService } from 'src/app/services/base.service';
 
 @Injectable()
-export class ContaService {
-    constructor(private http: HttpClient) {}
+export class ContaService extends BaseService {
+    //Quando extender uma classe chamar o super no construtor
+    constructor(private http: HttpClient) { super(); }
 
     
-    registrarUsuario(usuario: Usuario) {
+    registrarUsuario(usuario: Usuario) : Observable<Usuario> {
+        let response = this.http.post(this.UrlServiceV1 + 'nova-conta', usuario, this.ObterHeaderJson())
+        .pipe(
+            //Caminho feliz
+            map(this.extractData),
+            catchError(this.serviceError))
 
+        return response;
     }
 
     login(usuairo: Usuario) {
